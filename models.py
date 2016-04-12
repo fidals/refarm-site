ec648fc95a664c275a2b674b5e30a71b8353fb2a
duker <duker33@gmail.com>
2016-04-12 19:04:54 +0300

    [ 609 ] Pick blog app settings from protosite. This is site-specific settings

--- models.py
+++ models.py
@@ -1,16 +1,12 @@
 from django.db import models
+from django.conf import settings
 
 
 class Page(models.Model):
     "Page of site"
-    PAGE_TYPES = (
-        ('article', 'Статьи'),
-        ('news', 'Новости'),
-        ('navi', 'Навигация'),
-    )
     name = models.CharField(max_length=255)
     alias = models.CharField(max_length=255)
     is_text_published = models.BooleanField()
-    type = models.CharField(max_length=100, choices=PAGE_TYPES, default=PAGE_TYPES[0])
+    type = models.CharField(max_length=100, choices=settings.PAGE_TYPES, default=settings.PAGE_TYPES[0])
 
     def __str__(self): return self.name
