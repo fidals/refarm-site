1b7a3a944b9cf1fcc17f57368c145a2e2c17fe76
duker <duker33@gmail.com>
2016-05-17 16:38:29 +0300

    Add PyPI requirements at setup.py. Add our nonPyPI repos in requirements.txt Used price from this arcicle: https://caremad.io/2013/07/setup-vs-requirement/

--- setup.py
+++ setup.py
@@ -32,4 +32,8 @@ setup(
         'Topic :: Internet :: WWW/HTTP',
         'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
     ],
+    install_requires=[
+        'Django == 1.9.5',
+        'refarm-seo',
+    ],
 )
