ae53977dc954c3cb93834aa37e6f9932b89328ca
Artemiy <wertins71@gmail.com>
2018-06-21 08:21:53 +0300

    Move custom_page_url from test.pages.urls to pages.urls (#137)

--- tests/pages/urls.py
+++ tests/pages/urls.py
@@ -1,13 +1,7 @@
-from functools import partial
-
 from django.conf.urls import url, include
 
 from pages import views
-from pages.models import Page
-
-# @todo #343:30m Use custom_page_url for every custom page route.
-custom_page_url = partial(url, name=Page.CUSTOM_PAGES_URL_NAME)
-
+from pages.urls import custom_page_url
 
 urlpatterns = [
     url(r'^', include('pages.urls')),
