1bb80f774fd42892a6ac821bba50afb7df0cbb72
Artemiy Rodionov <wertins71@gmail.com>
2018-09-02 07:56:46 +0300

    Redesign pagination classes. Create a context class for pagination classes

---
+++ refarm_pagination/pagination.py
@@ -0,0 +1,53 @@
+from django.conf import settings
+from django.core import paginator
+
+
+class Page(paginator.Page):
+
+    def showed_number(self):
+        return (self.number - 1) * self.paginator.per_page + len(self)
+
+
+
+class Paginator(paginator.Paginator):
+
+    def page(self, number) -> Page:
+        """Raise Http404 instead of InvalidPage."""
+        try:
+            return super().page(number)
+        except paginator.InvalidPage:
+            raise http.Http404('Page does not exist')
+
+
+class NeighborPage:
+
+    def __init__(self, number):
+        self.number = number
+
+    def url(self, base_url):
+        return base_url if number == 1 else f'{base_url}?page={number}'
+
+
+class NeighborPages:
+
+    def __init__(self, page: paginator.Page):
+        self.page = page
+
+        self._index = page.number - 1
+        self._neighbor_bounds = settings.PAGINATION_NEIGHBORS // 2
+        self._neighbor_range = list(self.page.paginator.page_range)
+
+    def _neighbors(self, numbers):
+        neighbors = []
+        for number in numbers:
+            self.page.paginator.validate_number(number)
+            neighbors.append(NeighborPage(number))
+        return neighbors
+
+    def prev_neighbors(self):
+        numbers = self._neighbor_range[:self._index][-self._neighbor_bounds:]
+        return self._neighbors(numbers)
+
+    def next_neighbors(self):
+        numbers = self._neighbor_range[self._index + 1:][:self._neighbor_bounds]
+        return self._neighbors(numbers)
--- refarm_pagination/views.py
+++ refarm_pagination/views.py
@@ -1,37 +1,27 @@
-from django.conf import settings
-from django.core.paginator import Paginator, InvalidPage
+from refarm_pagination import Paginator, NeighborPages
 
 
-class PaginatorLinks:
+class PaginationContext:
 
-    def __init__(self, number, path, paginated: Paginator):
-        self.paginated = paginated
-        self.number = number
+    def __init__(self, path, objects, per_page):
         self.path = path
+        self.paginator = Paginator(objects, per_page)
 
-        self.index = number - 1
-        self.neighbor_bounds = settings.PAGINATION_NEIGHBORS // 2
-        self.neighbor_range = list(self.paginated.page_range)
+    def context(self):
+        page = self.paginator.page()
+        neighbords = NeighborPages(page)
 
-    def page(self):
-        try:
-            return self.paginated.page(self.number)
-        except InvalidPage:
-            raise http.Http404('Page does not exist')
+        prev_pairs = [
+            (prev.number, prev.url)
+            for prev in neighbords.prev_neighbors()
+        ]
+        next_pairs = [
+            (prev.number, prev.url)
+            for prev in neighbords.prev_neighbors()
+        ]
 
-    def showed_number(self):
-        return self.index * self.paginated.per_page + self.page().object_list.count()
-
-    def _url(self, number):
-        self.paginated.validate_number(number)
-        return self.path if number == 1 else f'{self.path}?page={number}'
-
-    def prev_numbers(self):
-        return self.neighbor_range[:self.index][-self.neighbor_bounds:]
-
-    def next_numbers(self):
-        return self.neighbor_range[self.index + 1:][:self.neighbor_bounds]
-
-    def number_url_map(self):
-        numbers = self.prev_numbers() + self.next_numbers()
-        return {number: self._url(number) for number in numbers}
+        return {
+            'page': page,
+            'prev_pairs':prev_pairs,
+            'next_pairs':next_pairs,
+        }
