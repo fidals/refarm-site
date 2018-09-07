ef0210ea404afc002c51151f505df4045327803a
Artemiy <wertins71@gmail.com>
2018-09-07 05:08:48 +0300

    se#561 Create logging for send_mail task. Set acks_late=True. (#176)
    * Create logging for send_mail task. Set True for ignore_result option.
    
    * Enable acks_late for send_mail
    
    * Add link to docs for acks_late

--- ecommerce/tasks.py
+++ ecommerce/tasks.py
@@ -1,8 +1,22 @@
 from __future__ import absolute_import, unicode_literals
+
 from celery import shared_task
+from celery.utils.log import get_task_logger
+from django.conf import settings
 from django.core.mail import send_mail as django_send_mail
 
+logger = get_task_logger(__name__)
 
-@shared_task(ignore_result=True)
+
+@shared_task(
+    # https://goo.gl/yAhUjJ
+    # a task will be acknowledged after the task has been executed,
+    # not just before (the default behavior) to reach reliability
+    acks_late=True,
+)
 def send_mail(*args, **kwargs):
+    logger.info(
+        f'Send mail from: {kwargs.get("from_email", settings.EMAIL_SENDER)}'
+        f' to {", ".join(kwargs.get("recipient_list", []))}'
+    )
     django_send_mail(*args, **kwargs)
