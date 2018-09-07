e7c3daf75bed8a814a5dc9bb859b1d79c352453c
Artemiy Rodionov <wertins71@gmail.com>
2018-09-07 04:51:47 +0300

    Enable acks_late for send_mail

--- ecommerce/tasks.py
+++ ecommerce/tasks.py
@@ -8,10 +8,14 @@ from django.core.mail import send_mail as django_send_mail
 logger = get_task_logger(__name__)
 
 
-@shared_task(ignore_result=False)
+@shared_task(
+    # a task will be acknowledged after the task has been executed,
+    # not just before (the default behavior) to reach reliability
+    acks_late=True,
+)
 def send_mail(*args, **kwargs):
     logger.info(
         f'Send mail from: {kwargs.get("from_email", settings.EMAIL_SENDER)}'
-        f' to {kwargs.get("recipient_list", "")}'
+        f' to {", ".join(kwargs.get("recipient_list", []))}'
     )
     django_send_mail(*args, **kwargs)
