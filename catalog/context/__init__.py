0373078cd4a47713f56c9a35d9d9db98d59a91c7
Artemiy Rodionov <wertins71@gmail.com>
2018-10-08 13:50:04 +0400

    Create context package

---
+++ catalog/context/__init__.py
@@ -0,0 +1,8 @@
+from .products import Products
+from .category import Category
+from .page import Page
+from .tags import Tags
+
+# @todo #183:60m Move context package to separate django app.
+
+# @todo #183:120m Compose context classes for catalog view.
