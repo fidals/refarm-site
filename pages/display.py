fd36af5b700a06d0a458d7890dcad8cc72720539
Artemiy Rodionov <wertins71@gmail.com>
2019-02-25 06:21:55 +0300

    Create __set_name__ function

--- pages/display.py
+++ pages/display.py
@@ -16,25 +16,48 @@ class Page:
     # Fields stored in DB. See class `pages.models.PageTemplate`
     STORED = ['name', 'h1', 'keywords', 'description', 'title', 'seo_text']
 
-    def __init__(self, page: 'pages.models.Page'=None, context: typing.Dict[str, typing.Any]=None):
+    def __init__(
+        self,
+        context: typing.Dict[str, typing.Any]=None,
+        *,
+        _page: 'pages.models.Page'=None,
+        _name='',
+    ):
         """
         Pass context at ctor, but not render method,
         because client code wants the same context for many different cases.
         """
-        self._page = page
-        self._global_context = defaultdict(dict)
-        if context:
-            self._global_context[page.id] = context
+        self._context = context or {}
+        self._page = _page
+        self._name = _name
+
+    def __set_name__(self, instance, name):
+        # get `Page` instace to create new `Page` instance with `name`
+        page = getattr(instance, name)
+        page_with_name = Page(page._context, _page=page._page, _name=name)
+        # Set new `Page` instance with name
+        setattr(instance, name, page_with_name)
 
     def __get__(self, instance: 'pages.models.Page', type_):
+        if instance and self._name in instance.__dict__:
+            return instance.__dict__[self._name]
         return Page(
-            instance,
-            {'page': instance, **self._global_context[instance.id]}
+            {'page': instance, **self._context},
+            _page=instance,
+            _name=self._name,
         )
 
     def __set__(self, instance: 'pages.models.Page', value: typing.Dict[str, typing.Any]):
-        self._page = instance
-        self._global_context[instance.id] = value
+        if not isinstance(value, dict):
+            raise ValueError(f'Value should be a dict')
+
+        if self._name in instance.__dict__:
+            # merge old context with new one
+            context = {**instance.__dict__[self._name]._context, **value}
+        else:
+            context = {**self._context, **value}
+
+        instance.__dict__[self._name] = Page(context, _page=instance, _name=self._name)
 
     def __getattr__(self, item):
         if item in self.STORED:
@@ -44,9 +67,7 @@ class Page:
 
     def render(self, field: str):
         return (
-            self._page.template.render_field(
-                field, context=self._global_context[self._page.id]
-            )
+            self._page.template.render_field(field, context=self._context)
             if self._page.template
             else getattr(self._page, field)
         )
