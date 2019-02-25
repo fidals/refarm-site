83dff44f63ccba6b9659797fa9d914c09a856c33
Artemiy Rodionov <wertins71@gmail.com>
2019-02-25 07:10:00 +0300

    Redesign Index classes

--- catalog/models_operations.py
+++ catalog/models_operations.py
@@ -1,11 +1,48 @@
+import abc
+import typing
+
 from django.db.migrations.operations.base import Operation
 
 # @todo #283:30m Group models.py, models_operations.py, models_expressions.py into the module.
 
 
-class AddIndexSQL(Operation):
+class IndexSQL(abc.ABC):
+
+    def __init__(self, name: str)
+        self.name = name
+
+    def _index_name(self, table: str):
+        return f'{table}_{self.name}_idx'
+
+    @abc.abstractmethod
+    def execute(self, table: str, schema_editor):
+        """Execute SQL operation."""
+
+
+class AddIndex(IndexSQL):
+
+    def __init__(self, name: str, columns: typing.List[str])
+        self.name = name
+        self.columns = columns
+
+    def execute(self, table, schema_editor):
+        schema_editor.execute(
+            f'CREATE INDEX {self._index_name(table_name)} ON {table}'
+            f'({", ".join(self.columns)});'
+        )
+
+
+class DropIndex:
+
+    def execute(self, table, schema_editor):
+        schema_editor.execute(
+            f'DROP INDEX {self._index_name(table)};'
+        )
+
+
+class IndexOperation(Operation):
     """
-    Create a B-Tree index by given columns or expressions.
+    Operate an index by given IndexSQL objects.
 
     Docs: https://docs.djangoproject.com/en/1.11/ref/migration-operations/#writing-your-own
     """
@@ -13,45 +50,83 @@ class AddIndexSQL(Operation):
     reduces_to_sql = True
     reversible = True
 
-    def __init__(self, name, columns, model_name):
+    def __init__(self, model_name, forward: IndexSQL, backward: IndexSQL):
         self.name = name
-        self.columns = columns
-        self.model_name = model_name
+        self.forward = forward
+        self.backward = backward
 
     def state_forwards(self, app_label, state):
         """We have to implement this method for Operation interface."""
-
-    def _index_name(self, table_name):
-        return f'{table_name}_{self.name}_idx'
 
     def database_forwards(self, app_label, schema_editor, _, to_state):
         to_model = to_state.apps.get_model(app_label, self.model_name)
         if self.allow_migrate_model(schema_editor.connection.alias, to_model):
             table_name = to_model._meta.db_table
-            schema_editor.execute(
-                f'CREATE INDEX {self._index_name(table_name)} ON {table_name}'
-                f'({", ".join(self.columns)});'
-            )
+            self.forward.operation(table_name, schema_editor)
 
     def database_backwards(self, app_label, schema_editor, from_state, _):
         from_model = from_state.apps.get_model(app_label, self.model_name)
         if self.allow_migrate_model(schema_editor.connection.alias, from_model):
             table_name = from_model._meta.db_table
-            schema_editor.execute(
-                f'DROP INDEX {self._index_name(table_name)};'
-            )
+            self.backward.operation(table_name, schema_editor)
 
     def describe(self):
-        return f'Create index {self.name} for {self.model_name}'
+        return f'Operate the index {self.name} for {self.model_name}'
+
+
+class RevertIndex(Operation):
+
+    reduces_to_sql = True
+    reversible = True
+
+    def __init__(self, operation: IndexOperation):
+        self.operation = operation
+
+    def state_forwards(self, app_label, state):
+        """We have to implement this method for Operation interface."""
+
+    def database_forwards(self, app_label, schema_editor, from_state, to_state):
+        self.operation.database_backwards(app_label, schema_editor, from_state, to_state)
+
+    def database_backwards(self, app_label, schema_editor, from_state, to_state):
+        self.operation.database_forwards(app_label, schema_editor, from_state, to_state)
+
+
 
 # Django doesn't provide ability to add hooks to makemigrations.
 # So we have to create migration files and add operations for
 # abstract classes (like Tag) manually.
-index_alphanumeric_tag_name = AddIndexSQL(
-    name='alphanumeric_name',
-    columns=[
-        "substring(name, '[a-zA-Zа-яА-Я]+')",
-        "(substring(name, '[0-9]+\.?[0-9]*')::float)"
-    ],
-    model_name='tag',
-)
+class IndexTagAlphanumeric:
+
+    ALPHANUMERIC_NAME = 'alphanumeric_name'
+    MODEL_NAME = 'tag'
+
+    def v1(self) -> typing.List[IndexOperation]:
+        return [IndexOperation(
+            model_name=self.MODEL_NAME,
+            forward=AddIndex(
+                name=self.ALPHANUMERIC_NAME,
+                columns=[
+                    "substring(name, '[a-zA-Zа-яА-Я]+')",
+                    "(substring(name, '[0-9]+\.?[0-9]*')::float)"
+                ],
+            ),
+            backward=DropIndex(name=self.ALPHANUMERIC_NAME),
+        )]
+
+    def v2(self) -> typing.List[IndexOperation]:
+        old = self.v1()[0]
+        return [
+            RevertIndex(old),
+            IndexOperation(
+                model_name=self.MODEL_NAME,
+                forward=AddIndex(
+                    name=self.ALPHANUMERIC_NAME,
+                    columns=[
+                        "substring(name, '[a-zA-Zа-яА-Я\s]+')",
+                        "(substring(name, '[0-9]+\.?[0-9]*')::float)"
+                    ],
+                ),
+                backward=DropIndex(name=self.ALPHANUMERIC_NAME),
+            )
+        ]
