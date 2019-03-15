33b3453949a18e5fc77d37a237a65ddaf256451d
duker33 <duker33@gmail.com>
2019-03-15 17:18:58 +0300

    #309  Improve context struct

--- catalog/context/__init__.py
+++
@@ -1,2 +0,0 @@
-from . import category, pages, products, tags
-
--- catalog/context/base.py
+++ catalog/context/base.py
@@ -41,3 +41,24 @@ class Products(Context):
         return {
             'products': self.products,
         }
+
+
+class Page(Context):
+
+    def __init__(self, page, tags: Tags):
+        self._page = page
+        self._tags = tags
+
+    def context(self) -> typing.ContextDict:
+        tags_qs = self._tags.qs()
+        # use dirty patch here, because it's the most simple method
+        # to make shared templates work.
+        # For example `templates/layout/metadata.html`.
+        self._page.display = {
+            'page': self._page,
+            'tag_titles': tags_qs.as_title(),
+            'tags': tags_qs,
+        }
+        return {
+            'page': self._page,
+        }
