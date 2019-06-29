17962167ac3cac4df80c433795d10519ab2f6b10
Artemiy Rodionov <wertins71@gmail.com>
2019-06-29 04:56:22 +0300

    Implement a failed test rerun feature

--- refarm_test_utils/runners.py
+++ refarm_test_utils/runners.py
@@ -9,10 +9,13 @@ from refarm_test_utils.results import TimedResult
 class RefarmTestRunner(DiscoverRunner):
     """Check every test is tagged and show slowest tests table."""
 
-    def __init__(self, *args, top_slow=0, check_tags=True, **kwargs):
+    def __init__(self, *args, top_slow=0, check_tags=True, rerun_failed=0,**kwargs):
         super().__init__(*args, **kwargs)
         self.top_slow = top_slow
         self.check_tags = check_tags
+
+        self.rerun_failed = rerun_failed
+        self.tests_to_rerun = []
 
     @classmethod
     def add_arguments(cls, parser):
@@ -25,6 +28,10 @@ class RefarmTestRunner(DiscoverRunner):
             '--disable-tags-check', action='store_false', dest='check_tags',
             help='Disables checking that each test is tagged.'
         )
+        parser.add_argument(
+            '--rerun-failed', nargs='?', const=1, type=int, default=0,
+            metavar='N', help='Runs failed tests while they occur. Gives N tries.',
+        )
 
     def build_suite(self, *args, **kwargs):
         suite = super().build_suite(*args, **kwargs)
@@ -35,8 +42,18 @@ class RefarmTestRunner(DiscoverRunner):
     def get_resultclass(self):
         return TimedResult if self.top_slow else super().get_resultclass()
 
-    def run_suite(self, *args, **kwargs):
-        result = super().run_suite(*args, **kwargs)
+    def run_suite(self, suite, **kwargs):
+        result = super().run_suite(suite, **kwargs)
+
+        if self.rerun_failed > 0 and self.suite_result(suite, result) > 0:
+            # @todo #stb-505:15m Decide whether we should rerun tests with errors or not.
+            self.tests_to_rerun = (
+                [f[0].id() for f in result.failures]
+                + [e[0].id() for e in result.errors]
+            )
+        else:
+            self.tests_to_rerun = []
+
         if self.top_slow:
             assert isinstance(result, TimedResult), result
 
@@ -54,6 +71,16 @@ class RefarmTestRunner(DiscoverRunner):
                 print(f'{i}. {elapsed:.2f} {name.splitlines()[0]}')
 
         return result
+
+    def run_tests(self, test_labels, extra_tests=None, **kwargs):
+        suite_result = super().run_tests(test_labels, extra_tests, **kwargs)
+        if self.tests_to_rerun:
+            delimiter = '\n\t- '
+            self.rerun_failed -= 1
+            print(f'\nRerun tries left: {self.rerun_failed}')
+            print(f'Rerun these failed tests:{delimiter}{delimiter.join(self.tests_to_rerun)}\n')
+            suite_result = self.run_tests(self.tests_to_rerun, **kwargs)
+        return suite_result
 
 
 def check_tagged_tests(suite):
